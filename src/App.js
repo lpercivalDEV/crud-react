30e6e0eeb014274a00cb037562974053fbcfa797
lpercivalDEV <lpercival.dev@gmail.com>
2020-03-04 20:03:14 -0500

    Add list and click events to items rendered from list

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  onClick() {
+    alert("dog");
+  }
+
   render() {
     const title = "React CRUD app - Keonda draft yay!";
 
     const anotherTitle = "Keonda - the new wave of connection"
 
     const list = [
-      "item 1, ",
-      "item 2, ",
-      "Another item, "
-    ]
+      "item 1",
+      "item 2",
+      "Another item"
+    ];
 
     return (
       <div className="App">
@@ -24,7 +28,9 @@ class App extends Component {
         <h2>
           {
             list.map(item => {
-              return item;
+              return (
+                <div onClick={this.onClick}>{item}</div>
+              );
             })
           }
         </h2>
